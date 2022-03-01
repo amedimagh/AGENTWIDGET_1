6c83adb6e48fa9f2caa4837793dd9308bea8a8a1
amedimagh <100612683+amedimagh@users.noreply.github.com>
2022-03-01 15:12:14 +0100

    Update agentwidget.js

--- agentwidget.js
+++ agentwidget.js
@@ -1,29 +1,27 @@
-
-
 function searchMovieName() { 
 
-    const moviename = document.getElementById('moviename');
-    const result =  document.getElementById('movieresult');
-    const url = 'https://www.omdbapi.com/?i=tt3896198&apikey=1e420c2d&t=' ;
+  const moviename = document.getElementById('moviename');
+  const result =  document.getElementById('movieresult');
+  const url = 'https://www.omdbapi.com/?i=tt3896198&apikey=1e420c2d&t=' ;
 
 
-    fetch(url+moviename.value)
-    .then(function(res) {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then(function(value) {
-      console.log(value);
-      result.innerText = JSON.stringify(value);
+  fetch(url+moviename.value)
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+    result.innerText = JSON.stringify(value);
 
 
 
-    })
-    .catch(function(err) {
-        console.log("error while fetching");
-    });
-  
+  })
+  .catch(function(err) {
+      console.log("error while fetching");
+  });
+
 
 }
 
@@ -33,75 +31,75 @@ function searchMovieName() {
 
 function searchLastMessage(){
 
-  const url = 'https://www.omdbapi.com/?i=tt3896198&apikey=1e420c2d&t=' ;
-  const response =  document.getElementById('search-by-msg-response');
-  const searchString = document.getElementById('search-by-msg-string');
-  const customerMessages = [];
-  var LastcustomerMessage = "";
-  var t ="";
-  lpTag.agentSDK.init();
-  
-  var updateCallback = function(data) {
-    var path = data.key;
-    var value = data.newValue;
-  console.log('update call back Path : ', value);
-  console.log('update call back Value : ', value);
-    value.forEach( elem => {
-      if(elem.source == 'visitor') {
-        customerMessages.push(elem.text);
-        LastcustomerMessage= customerMessages[customerMessages.length - 1];
-      }
-    });
-    console.log("customer messages: ",customerMessages);
-    console.log("LastcustomerMessage: ",LastcustomerMessage);
-  };
+const url = 'https://www.omdbapi.com/?i=tt3896198&apikey=1e420c2d&t=' ;
+const response =  document.getElementById('search-by-msg-response');
+const searchString = document.getElementById('search-by-msg-string');
+const customerMessages = [];
+var LastcustomerMessage = "";
 
+lpTag.agentSDK.init();
 
-  var notifyWhenDone = function(err) {
-    if (err) {
-      console.log("error when notifying");
+var updateCallback = function(data) {
+  var path = data.key;
+  var value = data.newValue;
+console.log('update call back Path : ', value);
+console.log('update call back Value : ', value);
+  value.forEach( elem => {
+    if(elem.source == 'visitor') {
+      customerMessages.push(elem.text);
+      LastcustomerMessage= customerMessages[customerMessages.length - 1];
     }
-    console.log("Bind Done");
-   
-  };
+  });
+  console.log("customer messages: ",customerMessages);
+  console.log("LastcustomerMessage: ",LastcustomerMessage);
 
+};
+
+
+var notifyWhenDone = function(err) {
+  if (err) {
+    console.log("error when notifying");
+  }
+  console.log("Bind Done");
+ 
+};
+
+
+
+  var pathToData = "chatTranscript.lines";
+  lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
   
-
-    var pathToData = "chatTranscript.lines";
-    lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
-    
-    
-    setTimeout(function () {
-      // do what you want
-         const url = 'https://www.omdbapi.com/?i=tt3896198&apikey=1e420c2d&t=' ;
-          searchString.innerText = "Search String: "+ customerMessages[customerMessages.length - 1];
-           console.log("Search string:" , searchString)
-           
-          t = customerMessages[customerMessages.length - 1];
-           console.log("tttttttttt:" , t)
-        
-        
-          fetch(url+t)
-           .then(res => res.json())
-              .then((out) => {
-                  console.log('Output: ', out);
-                  response.innerText = 'Search by Message Response: '+JSON.stringify(out);
-              
-              }).catch(err => errorTxt.innerText = err);
-          
-    }, 6000);
-
-
   
+  setTimeout(function () {
+    // do what you want
+       const url = 'https://www.omdbapi.com/?i=tt3896198&apikey=1e420c2d&t=' ;
+        searchString.innerText = "Search String: "+ LastcustomerMessage;
+         console.log("Search string:" , searchString)
+      
+        fetch(url+LastcustomerMessage)
+    .then(function(res) {
+    if (res.ok) {
+        return res.json();
+    }
+    })
+    .then(function(value) {
+    console.log(value);
+    response.innerText = JSON.stringify(value);
+
+    })
+    .catch(function(err) {
+    console.log("error while fetching");
+}); }, 5000);
+
+
+
 }
-
 
 
 function resetValues(){
-    
-  const form = document.getElementById('search-by-title-form').reset();
   
-  
+const form = document.getElementById('search-by-title-form').reset();
+
+
 
 }
-
